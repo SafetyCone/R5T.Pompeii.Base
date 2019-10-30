b2a7ff7b1c7c876b047f825989276b24af675074
MinexAutomation <minex.automation@gmail.com>
2019-10-29 22:42:39 -0700

    Added MANY entry-point project specific service interface definitions.

---
+++ source/R5T.Pompeii.Base/Code/Services/Interfaces/IEntryPointProjectBuildOutputFrameworkDirectoryPathProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.Pompeii
+{
+    public interface IEntryPointProjectBuildOutputFrameworkDirectoryPathProvider
+    {
+        string GetEntryPointProjectBuildOutputFrameworkDirectoryPath();
+    }
+}
---
+++ source/R5T.Pompeii.Base/Code/Services/Interfaces/ISolutionFileNameProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.Pompeii
+{
+    public interface ISolutionFileNameProvider
+    {
+        string GetSolutionFileName(string solutionDirectoryPath);
+    }
+}
