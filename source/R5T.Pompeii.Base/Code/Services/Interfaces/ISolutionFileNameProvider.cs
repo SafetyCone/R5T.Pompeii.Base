62cbf0a3891a8a42594c3e318e595f2fb545f280
MinexAutomation <minex.automation@gmail.com>
2020-02-18 23:36:42 -0800

    Add documentation.

--- source/R5T.Pompeii.Base/Code/Services/Interfaces/ISolutionFileNameProvider.cs
+++ source/R5T.Pompeii.Base/Code/Services/Interfaces/ISolutionFileNameProvider.cs
@@ -3,6 +3,9 @@
 
 namespace R5T.Pompeii
 {
+    /// <summary>
+    /// Provides a solution file name.
+    /// </summary>
     public interface ISolutionFileNameProvider
     {
         string GetSolutionFileName(string solutionDirectoryPath);
