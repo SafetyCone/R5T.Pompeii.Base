0bf87b5f2a23b3cb8113530efcdbd4c46bbf9dd6
davidcoats <david@getsafetycone.com>
2022-04-27 15:33:57 -0700

    Interim changes.

---
+++ source/R5T.Pompeii.Base/Code/Services/Definitions/IEntryPointProjectBinariesDirectoryPathProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using R5T.T0064;
+
+
+namespace R5T.Pompeii
+{
+    [ServiceDefinitionMarker]
+    public interface IEntryPointProjectBinariesDirectoryPathProvider : IServiceDefinition
+    {
+        string GetEntryPointProjectBinariesDirectoryPath();
+    }
+}
---
+++ source/R5T.Pompeii.Base/Code/Services/Definitions/ISolutionFileNameProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using R5T.T0064;
+
+
+namespace R5T.Pompeii
+{
+    /// <summary>
+    /// Provides a solution file name.
+    /// </summary>
+    [ServiceDefinitionMarker]
+    public interface ISolutionFileNameProvider : IServiceDefinition
+    {
+        string GetSolutionFileName(string solutionDirectoryPath);
+    }
+}
